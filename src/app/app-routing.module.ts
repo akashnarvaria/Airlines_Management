40492f6db122e34659080feee7a042caeee9af22
akashnarvaria <akashnarvaria9@gmail.com>
2021-04-01 00:02:12 +0530

    fix: login form added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'staff', loadChildren: () => import('./modules/staff/staff.module').then(m => m.StaffModule) }, { path: 'admin', loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule) }, { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
